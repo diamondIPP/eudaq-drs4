f1372196ed6c0f95d7761786e9763c3b814ecb34
cdorfer <cdorfer@phys.ethz.ch>
2016-08-03 15:56:16 +0200

    TU online monitor fixed (?)

--- producers/tu/src/TUProducer.cpp
+++ producers/tu/src/TUProducer.cpp
@@ -90,7 +90,7 @@ void TUProducer::MainLoop(){
 				beam_current[1] = rd->beam_curent; //save new
 				time_stamps[0] = time_stamps[1]; //save old timestamp for frequency calculations
 				time_stamps[1] = rd->time_stamp; //save new
-				cal_beam_current = SlidingWindow(0.01*((beam_current[1]-beam_current[0])/(time_stamps[1] - time_stamps[0])));
+				cal_beam_current = 1000*SlidingWindow(0.01*((beam_current[1]-beam_current[0])/(time_stamps[1] - time_stamps[0])));
 
 				for(int idx=0; idx<10; idx++){
 					//check if there was a fallover
