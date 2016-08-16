576a136475030b0605b9fe2a857bc2b135094315
michareichmann <micha.reichmann@gmail.com>
2016-08-16 09:42:49 +0200

    adding pulser rate histo and use it to check for beam interrupts in the alignment plots

--- monitors/onlinemon/include/EventAlignmentHistos.hh
+++ monitors/onlinemon/include/EventAlignmentHistos.hh
@@ -26,6 +26,7 @@ class EventAlignmentHistos{
 protected:
     TH1 * _Alignment;
     TH1 * _AlignmentPlus1;
+    TH1 * _PulserRate;
     TH2I * _IsAligned;
     TH2I * _IsAlignedPlus1;
     uint16_t _lastNClusters;
@@ -38,15 +39,17 @@ public:
     void Reset();
     TProfile * getAlignmentHisto() { return (TProfile*)_Alignment; }
     TProfile *  getAlignmentPlus1Histo() { return (TProfile*)_AlignmentPlus1; }
+    TProfile *  getPulserRate() { return (TProfile*)_PulserRate; }
     TH2I * getIsAlignedHisto() { return _IsAligned; }
     TH2I * getIsAlignedPlus1Histo() { return _IsAlignedPlus1; }
 
 private:
-    uint16_t bin_size;
-    uint32_t max_event_number;
-    TProfile * init_profile(std::string, std::string);
+    const uint16_t _bin_size;
+    const uint32_t max_event_number;
+    TProfile * init_profile(std::string, std::string, uint16_t bin_size=0, std::string ytit="Fraction of Hits @ Pulser Events [%]");
     TH2I * init_th2i(std::string, std::string);
-    void FillIsAligned(TProfile*, TH2I*);
+    void FillIsAligned(TProfile*, TH2I*, TProfile*);
+    void ResizeObjects(uint32_t);
 };
 
 
