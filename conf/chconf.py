ea70b4923802ae84d8ae45c60528d261ff5ee6ca
cdorfer <cdorfer@phys.ethz.ch>
2016-10-19 14:44:19 +0200

    status of config files for pad setup

--- conf/chconf.py
+++ conf/chconf.py
@@ -15,14 +15,14 @@ for name in glob('{d}*.conf'.format(d=args.digitizer.upper())):
     f = open(name, 'r+')
     lines = []
     for line in f.readlines():
-        if line.lower().startswith(option):
+        if line.lower().startswith(option.lower()):
             data = line.replace('#', '=').split('=')
             for i in xrange(len(data)):
                 data[i] = data[i].strip(' \t')
             if value is None:
                 value = raw_input('Which value do you wish to put? (the current one is {0}): '.format(data[1]))
             comment = '  # {0}'.format(data[2]) if len(data) > 2 else ''
-            line = '{0} = {1}{2}'.format(data[0], value, comment)
+            line = '{0} = {1}{2}{3}'.format(data[0], value, comment, '\n' if not comment else '')
         lines.append(line)
     f.seek(0)
     f.writelines(lines)
