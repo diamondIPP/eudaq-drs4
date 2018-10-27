314a7107cdeac87be1bd075a61ffba27cfe6565e
michareichmann <remichae@phys.ethz.ch>
2018-10-27 02:57:08 +0200

    adding shifter bot

---
+++ shifterBot/spreadsheet.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# --------------------------------------------------------
+#       small script to read data from google spreadsheet
+# created on August 30th 2018 by M. Reichmann (remichae@phys.ethz.ch)
+# --------------------------------------------------------
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def load_sheet():
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/testbeam/sdvlp/eudaq-drs4/shifterBot/client_secret.json', scope)
+    client = gspread.authorize(creds)
+    return client.open_by_key('1EC2ZlWRUOo6VmfrQKcpWxcF8dX3SOBiXF51LgPqI4Dw').sheet1
+
+
+def num2col(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+def col2num(col):
+    return reduce(lambda x, y: x * 26 + y, [ord(c.upper()) - ord('A') + 1 for c in col])
+
+
+def get_first_unfilled(sheet, col):
+    cols = sheet.col_values(col2num(col))
+    last_filled = len(cols) - cols[::-1].index('TRUE')
+    return cols.index('FALSE', last_filled) + 1  # return the last index that is "TRUE"
+
+
+if __name__ == '__main__':
+    s = load_sheet()
---
+++ shifterBot/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+
+
+GREEN = '\033[92m'
+ENDC = '\033[0m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+
+
+def get_t_str():
+    return datetime.now().strftime('%H:%M:%S')
+
+
+def info(msg, overlay=False, prnt=True):
+    if prnt:
+        print '{ov}{head} {t} --> {msg}'.format(t=get_t_str(), msg=msg, head='{}INFO:{}'.format(GREEN, ENDC), ov='\033[1A\r' if overlay else '')
+
+
+def print_banner(msg, symbol='=', new_lines=True):
+    print '{n}{delim}\n{msg}\n{delim}{n}'.format(delim=(len(str(msg)) + 10) * symbol, msg=msg, n='\n' if new_lines else '')
