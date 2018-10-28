4180b02360bdf0a728234bda7aadeb034940460e
michareichmann <remichae@phys.ethz.ch>
2018-10-29 00:17:23 +0100

    add method to relaod email server

--- shifterBot/Email.py
+++ shifterBot/Email.py
@@ -4,6 +4,8 @@ from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from base64 import b64decode
+from time import time
+from utils import *
 
 
 class Email:
@@ -11,8 +13,10 @@ class Email:
     def __init__(self):
         self.MyAddress = 'micha.reichmann@gmail.com'
         self.MyUserName = 'micha.reichmann'
-        self.Recipients = [self.MyAddress, 'remichae@phys.ethz.ch', 'sandiego@phys.ethz.ch', 'dhits@ethz.ch']
+        self.Recipients = [self.MyAddress, 'sandiego@phys.ethz.ch', 'dhits@ethz.ch']
         self.Server = self.load_server()
+        self.TStart = time()
+        self.Reloads = 0
 
     @staticmethod
     def __get_pw():
@@ -36,6 +40,12 @@ class Email:
     def send_message(self, subject, msg):
         text = self.prepare_message(subject, msg).as_string()
         self.Server.sendmail(self.MyUserName, self.Recipients, text)
+
+    def reload_server(self):
+        if (time() - self.TStart) / (55 * 60) - 1 > self.Reloads:
+            info('reloading server ...\n')
+            self.Server = self.load_server()
+            self.Reloads += 1
 
 
 if __name__ == '__main__':
