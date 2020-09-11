10999df2e680d700816287113bc10df64c874503
michareichmann <micha.reichmann@gmail.com>
2020-09-11 14:37:34 +0200

    fix iclix scripts

--- scripts/iclix.py
+++ scripts/iclix.py
@@ -9,21 +9,19 @@ from utils import *
 from os.path import expanduser
 from subprocess import check_call
 
-parser = ArgumentParser()
-parser.add_argument('data', nargs='?', default='')
-parser.add_argument('-T', '--trim', nargs='?', default='')
-args = parser.parse_args()
 
-config = load_config()
-path = expanduser(join('~', config.get('MAIN', 'software directory'), 'pxar', 'python', 'iCLIX.py'))
-
-
-def run_wbc(data=None, trim=None):
-    data = args.data if data is None else data
-    trim = args.trim if trim is None else trim
-    cmd = 'python {} {} -wbc -T {}'.format(path, data, trim)
+def run_wbc(data, trim=None):
+    config = load_config()
+    path = expanduser(join('~', config.get('MAIN', 'software directory'), 'pxar', 'python', 'iCLIX.py'))
+    trim = '' if trim is None else '-T {}'.format(trim)
+    cmd = 'python {} {} -wbc {}'.format(path, data, trim)
     check_call(cmd.split())
 
 
 if __name__ == '__main__':
-    run_wbc()
+    parser = ArgumentParser()
+    parser.add_argument('data', nargs='?', default='')
+    parser.add_argument('-T', '--trim', nargs='?', default='')
+    args = parser.parse_args()
+
+    run_wbc(args.data, args.trim)
